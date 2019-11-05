fa49be7cf76a6a550e9b8d809ae04b61b1dcf2a8
Ekaterina-C <innocentdaiver@gmail.com>
2019-11-05 16:34:45 -0500

    Initial commit

---
+++ src/test/java/com/cbt/utilities/BrowserFactory.java
@@ -0,0 +1,33 @@
+package com.cbt.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class BrowserFactory {
+    public static WebDriver getDriver(String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            WebDriver driver = new ChromeDriver();
+            return driver;
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            WebDriver driver = new FirefoxDriver();
+            return driver;
+        } else if (browser.equalsIgnoreCase("edge")) {
+            System.setProperty("WebDriver.ie.driver", "C:\\Users\\Екатерина Сергеевна\\MicrosoftWebDriver.exe");
+            WebDriver driver = new EdgeDriver();
+            return driver;
+        } else if (browser.equalsIgnoreCase("safari")) {
+            return null;
+        }else{
+            return null;
+        }
+
+    }
+
+}
+
